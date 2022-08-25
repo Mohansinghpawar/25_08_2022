f012cd66f1f9833d63469cf8582cf29b79b734d6
Mohansinghpawar <88230569+Mohansinghpawar@users.noreply.github.com>
2022-08-25 14:52:25 +0530

    Create GetSet.java

---
+++ GetSet.java
@@ -0,0 +1,41 @@
+public class GetSet{
+	private int id;
+	private String firstName;
+	private String lastName;
+	private double marks;
+	private String schoolName;
+	public String getSchoolName() {
+		return schoolName;
+	}
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String longName) {
+		this.lastName = longName;
+	}
+	public double getMarks() {
+		return marks;
+	}
+	public void setMarks(double marks) {
+		this.marks = marks;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+	}
+}
